0b9487ef1acec53722db33992faba9e87c8b5b42
sankeyangshu <1260323835@qq.com>
2025-08-11 19:05:43 +0800

    chore: update deps

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,29 +5,7 @@ export default defineConfig(
   {
     rules: {
       'perfectionist/sort-objects': 'off',
+      '@eslint-community/eslint-comments/no-unlimited-disable': 'off',
     },
   }
 );
-
-// {
-//   rules: {
-//     // '@typescript-eslint/ban-types': 'off',
-//     // '@typescript-eslint/no-explicit-any': 'off',
-//     // '@typescript-eslint/promise-function-async': 'off',
-// 'vue/component-name-in-template-casing': [
-//   'warn',
-//   'PascalCase',
-//   {
-//     ignores: ['/^icon-/'],
-//     registeredComponentsOnly: false,
-//   },
-// ],
-// 'vue/multi-word-component-names': [
-//   'warn',
-//   {
-//     ignores: ['index', 'App', 'Register', '[id]', '[url]'],
-//   },
-// ],
-//     'unocss/order-attributify': 'off',
-//   },
-// }
