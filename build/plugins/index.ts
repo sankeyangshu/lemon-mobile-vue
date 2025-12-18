a18506dbedafdf10bf564524f39a04f10ad1ad99
sankeyangshu <1260323835@qq.com>
2025-12-18 08:58:31 +0800

    chore: update environment configurations and remove unused files

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -1,40 +1,37 @@
-import tailwindcss from '@tailwindcss/vite';
-import Vue from '@vitejs/plugin-vue';
-import ViteRestart from 'vite-plugin-restart';
-import VueDevtools from 'vite-plugin-vue-devtools';
-import { configHtmlPlugin } from './html';
-import { configInfoPlugin } from './info';
-import { configElegantRouter } from './router';
-import { configUnPlugin } from './unplugin';
 import type { PluginOption } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import viteRestart from 'vite-plugin-restart';
+import vueDevtools from 'vite-plugin-vue-devtools';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { setupHtmlPluginConfig } from './html';
+import { setupBuildInfoPluginConfig } from './info';
 
 /**
  * 配置 vite 插件
  * @param viteEnv vite 环境变量配置文件键值队 object
- * @param isBuild 是否是打包模式
+ * @param lastBuildTime 最后编译时间
  * @returns vitePlugins[]
  */
-export const createVitePlugins = (viteEnv: Env.ImportMeta, isBuild: boolean) => {
+export function createVitePlugins(_viteEnv: Env.ImportMeta, lastBuildTime: string) {
   const vitePlugins: (PluginOption | PluginOption[])[] = [
-    Vue(),
+    vue(),
 
-    configElegantRouter(),
-
-    VueDevtools(),
+    vueDevtools(),
 
     tailwindcss(),
 
-    ...configUnPlugin(viteEnv),
+    tsconfigPaths(),
 
     // 通过这个插件，在修改vite.config.ts文件则不需要重新运行也生效配置
-    ViteRestart({
+    viteRestart({
       restart: ['vite.config.ts'],
     }),
 
-    configHtmlPlugin(viteEnv, isBuild),
+    setupBuildInfoPluginConfig(),
 
-    configInfoPlugin(),
+    setupHtmlPluginConfig(lastBuildTime),
   ];
 
   return vitePlugins;
-};
+}
--- build/plugins/info.ts
+++ build/plugins/info.ts
@@ -1,14 +1,10 @@
-import boxen, { type Options as BoxenOptions } from 'boxen';
-import dayjs, { type Dayjs } from 'dayjs';
-import duration from 'dayjs/plugin/duration';
-import gradient from 'gradient-string';
-import { getPackageSize } from '../config';
+import type { Options as BoxenOptions } from 'boxen';
 import type { Plugin } from 'vite';
-
-dayjs.extend(duration);
+import boxen from 'boxen';
+import gradient from 'gradient-string';
 
 const welcomeMessage = gradient(['#EACA44', 'magenta']).multiline(
-  `您好! 欢迎使用 lemon-vue 开源项目\n我们为您精心准备了精美的保姆级文档\nhttps://sankeyangshu.github.io/lemon-template-docs/vue/`
+  `您好! 欢迎使用 lemon-mobile-vue 开源项目\n我们为您精心准备了精美的保姆级文档\nhttps://lemon-template-docs.vercel.app/mobile-vue/`,
 );
 
 const boxenOptions: BoxenOptions = {
@@ -21,48 +17,13 @@ const boxenOptions: BoxenOptions = {
  * config build info plugin
  * @descCN 配置打包信息插件
  */
-export function configInfoPlugin(): Plugin {
-  let config: { command: string };
-  let startTime: Dayjs;
-  let endTime: Dayjs;
-  let outDir: string;
+export function setupBuildInfoPluginConfig(): Plugin {
   return {
     name: 'vite:buildInfo',
-
-    configResolved(resolvedConfig) {
-      config = resolvedConfig;
-      outDir = resolvedConfig.build?.outDir ?? 'dist';
-    },
 
     buildStart() {
       // eslint-disable-next-line no-console
       console.log(boxen(welcomeMessage, boxenOptions));
-      if (config.command === 'build') {
-        startTime = dayjs(new Date());
-      }
-    },
-
-    closeBundle() {
-      if (config.command === 'build') {
-        endTime = dayjs(new Date());
-
-        getPackageSize({
-          folder: outDir,
-          callback: (size: string | number) => {
-            // eslint-disable-next-line no-console
-            console.log(
-              boxen(
-                gradient(['#EACA44', 'magenta']).multiline(
-                  `🎉 恭喜打包完成（总用时${dayjs
-                    .duration(endTime.diff(startTime))
-                    .format('mm分ss秒')}，打包后的大小为${size}）`
-                ),
-                boxenOptions
-              )
-            );
-          },
-        });
-      }
     },
   };
 }
