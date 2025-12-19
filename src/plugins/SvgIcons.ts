0ecda595034c0d74dab20c87344bff1f0bf6a507
sankeyangshu <1260323835@qq.com>
2025-12-19 09:27:41 +0800

    feat: add custom error pages for 403, 404, and 500 errors

--- src/plugins/SvgIcons.ts
+++
@@ -1,9 +0,0 @@
-import SvgIcon from '@/components/SvgIcon/index.vue'; // svg图标组件
-import type { App } from 'vue';
-
-/**
- * setup svg icon (导入svg图标)
- */
-export default (app: App<Element>) => {
-  app.component('SvgIcon', SvgIcon);
-};
